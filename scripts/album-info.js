ede38c24d277f316f7c72c5221fe73c95dda2bfb
mbedree <mabedree@gmail.com>
2017-09-25 18:03:19 -0400

    Add artist and release info

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
   $('#album-title').text(album.title);
   $('img#album-cover-art').attr('src',album.albumArtUrl);
+  //artist
+  $('.artist').text(album.artist);
+  //releaseInfo
+  $('#release-info').text(album.releaseInfo);
 });
