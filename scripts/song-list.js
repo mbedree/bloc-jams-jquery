d6000e2b4d8859bcb742697c940cdede0ec12244
mbedree <mabedree@gmail.com>
2017-09-26 07:56:57 -0400

    Add table

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
   album.songs.forEach( (song, index) => {
-    
+    song.element = $(`
+      <tr>
+        <td>index</td>
+        <td>song.title</td>
+        <td>song.duration</td>
+      </tr>
+      `)
   )};
 });
