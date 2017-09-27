1628e1ebc9723f16cab68ee75e0329245acec97e
mbedree <mabedree@gmail.com>
2017-09-27 12:17:14 -0400

    Add event for previous button

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -4,14 +4,27 @@ $(document).ready(function(){
     $(this).attr('playState', player.playState);
   });
 
+  //next button
   $('button#next').click(function(){
     if(player.playState !== 'playing') {return;}
 
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
     if(nextSongIndex >= album.songs.length) {return}
-    
+
     const nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
+  });
+
+  //previous button
+  $('button#previous').click(function(){
+    if(player.playState !== 'playing') {return}
+
+    let currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    let previousSongIndex = currentSongIndex - 1;
+    if(previousSongIndex < 0) {console.log("asdf");return}
+
+    let previousSong = album.songs[previousSongIndex];
+    player.playPause(previousSong);
   });
 });
