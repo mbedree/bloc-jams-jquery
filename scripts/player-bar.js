ff5a7a002be4af6aa9bd13183dd184b4553d4e00
mbedree <mabedree@gmail.com>
2017-09-27 13:32:21 -0400

    Add display for current time of playing song

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -36,6 +36,7 @@ $(document).ready(function(){
      const currentTime = player.getTime();
      const duration = player.getDuration();
      const percent = (currentTime / duration) * 100;
+     $('#time-control .current-time').text(currentTime);
      $('#time-control input').val(percent);
    }, 1000);
  });
