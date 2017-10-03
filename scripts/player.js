19dbc63adf7b3411dd81157fa361941270d4b117
mbedree <mabedree@gmail.com>
2017-10-03 10:02:14 -0400

    format player bar time to 0:00

--- scripts/player.js
+++ scripts/player.js
@@ -58,6 +58,10 @@ class Player {
     var divisor_for_seconds = divisor_for_minutes % 60;
     var seconds = Math.ceil(divisor_for_seconds);
 
+    if(seconds < 10){
+      seconds = "0" + seconds;
+    }
+
     var obj = {
         "h": hours,
         "m": minutes,
